6c11c9ba1bdb10af2d329e31d45fc8ca52642e20
imkeunho@naver.com <imkeunho@naver.com>
2026-02-02 22:50:38 +0900

    webApi 개발 MemberApi 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,11 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
+
     annotationProcessor("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+
+    testCompileOnly("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.junit-pioneer:junit-pioneer:2.3.0")
